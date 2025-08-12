86565b4d37af76c6f1e879cb32cc93a3b8cab72a
Benjamin Piatkowski <58381976+ben4235@users.noreply.github.com>
2025-08-12 15:32:22 -0700

    Create trivia.ts

---
+++ src/config/trivia.ts
@@ -0,0 +1,12 @@
+export const TriviaFacts: string[] = [
+  'Honey never spoils; archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still edible.',
+  'Bananas are berries, but strawberries are not.',
+  'Octopuses have three hearts.',
+  'A day on Venus is longer than its year.',
+  'There are more possible iterations of a game of chess than there are atoms in the known universe.',
+  'Wombat poop is cube-shaped.',
+  'The longest English word has 189,819 letters; it\'s the name of a protein known as Titin.',
+  'Rainbows are full circles; we usually see only half because the horizon blocks the rest.',
+  'Humans share 50% of their DNA with bananas.',
+  'The Eiffel Tower can be 15 cm taller during the summer due to thermal expansion.'
+];
