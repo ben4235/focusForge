c3d609abac22b30c420619cf67ac132df067adca
Benjamin Piatkowski <58381976+ben4235@users.noreply.github.com>
2025-08-12 15:28:57 -0700

    Update upgrades.ts

--- src/config/upgrades.ts
+++ src/config/upgrades.ts
@@ -3,7 +3,6 @@ export type UpgradeId =
   | 'reload_speed'
   | 'multi_shot'
   | 'spread'
-  | 'player_damage' // reserved for future
   | 'bullets_per_shot'
   | 'attack_speed';
 
@@ -21,41 +20,53 @@ export const UpgradePool: UpgradeDef[] = [
     name: 'Sharpened Rounds',
     desc: '+2 bullet damage',
     cost: 30,
-    apply: (s)=>{ s.player.bulletDamage += 2; }
+    apply: (s) => {
+      s.player.bulletDamage += 2;
+    }
   },
   {
     id: 'reload_speed',
     name: 'Quick Mag',
     desc: 'Reload 10% faster',
     cost: 30,
-    apply: (s)=>{ s.player.reload *= 0.9; }
+    apply: (s) => {
+      s.player.reload *= 0.9;
+    }
   },
   {
     id: 'multi_shot',
     name: 'Forked Barrel',
     desc: '+1 projectile per shot',
     cost: 40,
-    apply: (s)=>{ s.player.bulletsPerShot += 1; }
+    apply: (s) => {
+      s.player.bulletsPerShot += 1;
+    }
   },
   {
     id: 'spread',
     name: 'Tighter Spread',
     desc: '-20% spread',
     cost: 25,
-    apply: (s)=>{ s.player.spreadRadians *= 0.8; }
+    apply: (s) => {
+      s.player.spreadRadians *= 0.8;
+    }
   },
   {
     id: 'bullets_per_shot',
     name: 'Cluster Volley',
     desc: '+2 projectiles',
     cost: 60,
-    apply: (s)=>{ s.player.bulletsPerShot += 2; }
+    apply: (s) => {
+      s.player.bulletsPerShot += 2;
+    }
   },
   {
     id: 'attack_speed',
     name: 'Honed Reflexes',
     desc: '+15% attack speed',
     cost: 50,
-    apply: (s)=>{ s.player.attackSpeed *= 1.15; }
+    apply: (s) => {
+      s.player.attackSpeed *= 1.15;
+    }
   }
 ];
